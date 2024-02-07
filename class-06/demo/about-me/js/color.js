4be3be0d4ff45dd047ec472bfa7ba6bb9777056d
John Cokos <highlander.44@gmail.com>
2024-02-07 15:18:27 -0800

    Adds JS Examples

---
+++ class-06/demo/about-me/js/color.js
@@ -0,0 +1,3 @@
+let color = prompt("What color would you paint your house if you could?");
+
+document.write("Cool, I like " + color + " too.");
---
+++ class-06/demo/about-me/js/javascript-demo.js
@@ -0,0 +1,30 @@
+// What is code all about?
+// INPUT then PROCESS then OUTPUT
+// If this, then that (conditional logic)
+
+// Variables
+
+// Declare a variable called "wife" and assign it the value of "Cathy"
+// var, let, const
+let wife = "Cathy";
+
+// Print whatever is inside the () to the console
+console.log(wife);
+
+// Variables can be changed (replaced)
+wife = "Judy";
+
+console.log(wife);
+
+// Conditional logic -- IFTTT
+// Checks the status of whatever is in the () and if it's true, runs the {}
+//    If it's false, runs whatever is in the else {}
+// If the age is > 50, make fun of the person, otherwise, tease them
+let age = 55;
+
+if( age > 50 ) {
+  console.log("LOL, you're on the downslope!");
+} else {
+  console.log("Enjoy your youth while it lasts!");
+}
+
